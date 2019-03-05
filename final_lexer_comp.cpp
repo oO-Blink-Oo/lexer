89ae8dc31bc77254c8fc63600d13575dca2d8685
mesorangerxx <lqtruongnguyen@gmail.com>
2019-03-04 22:18:13 -0800

    WE DONE. almost, missing reals

--- final_lexer_comp.cpp
+++ final_lexer_comp.cpp
@@ -7,7 +7,6 @@
 
 using namespace std;
 
-enum transitions { INITIAL = 0, LETTER, DIGIT, DOT, EXPOINT, PUNCT, SPACE };
 
 struct tokenType {
 	string token; // token name for the lexeme
@@ -16,7 +15,8 @@ struct tokenType {
 
 };
 
-int stateFSM[13][24] = { //Fix table
+//FSM table
+int stateFSM[13][24] = { 
  /*state 0*/  {1, 3, 2 ,5, 6, 0, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 8, 8, 8, 8, 8, 8, 8, 8},
 /*state 1*/   {1, 11, 12, 2, 2, 2, 9, 2, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10},
 /*Fstate 2*/  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -48,13 +48,17 @@ int main() {
 	string stringExp = "";
 	vector<tokenType> tokens;
 
+	cout << setw(15) << left << "TOKENS:" <<  setw(18) << right << "LEXEMES:" << endl;
+	cout << endl;
+
 	while (getline(inFile, stringExp)) { //getline(inFile,stringExp) 
 		tokens = lexer(stringExp);
 
 		//print out tokens vector
 		for (size_t i = 0; i < tokens.size(); ++i) {
-
-			cout << tokens[i].lexemeName << " = " << tokens[i].token << endl;
+			if (tokens[i].lexemeName != "COMMENT") {
+				cout << setw(15) << left << tokens[i].lexemeName << " =" << setw(15) << right << tokens[i].token << endl;
+			}
 
 		}
 	}
@@ -64,17 +68,16 @@ int main() {
 	return 0;
 }
 
-//missing else and keyword large
+
 vector<tokenType> lexer(string words) {
 	tokenType acc; //the token struct we are working with
-	vector<tokenType> tokens; // will be returned
+	vector<tokenType> tokens; // vector filled with tokens to output in the main function
 	char currentChar = ' ';
 	int col = 0;			  // column number
 	int currentState = 0;	  //initial state
-	//int prevState = 0;
 	string currentToken = "";
-	vector<string> keywords = { "int","float","bool","if","else","then","do","while","whileend","do","doend","for","and","or","function" };
-	bool isKey = false;
+	vector<string> keywords = { "int","float","bool","if","else","then","do","while","whileend","do","doend","for","and","or","function" }; //vector of keywords in order to differentiate between identifier and keyword
+	bool isKey = false; //boolean key to determine if the current token is a keyword or identifier
 
 	//READS WORDS CHARACTER BY CHARACTER
 	for (size_t i = 0; i < words.length(); i++) {
@@ -87,14 +90,14 @@ vector<tokenType> lexer(string words) {
 
 		currentState = stateFSM[currentState][col];
 
-
+		//finishing states at 2,4,7-10,12
 		switch (currentState) {
-		case 0: //TODO, this case is only hit when your initial char is a space when you're in the initial state
+		case 0: //if the state is a space, break out of the switch
 			break;
 		case 1:
-			currentToken += currentChar;
+			currentToken += currentChar; //adds a validated character into the token for it to be pushed into the tokens vector later
 			break;
-		case 2: //keyword
+		case 2: //keyword state, branches out to identifier if isKey is false
 			for (int i = 0; i < keywords.size(); i++) {
 				if (currentToken == keywords[i]) {
 					acc.token = currentToken;
@@ -110,8 +113,6 @@ vector<tokenType> lexer(string words) {
 				acc.lexemeName = getLexemeName(acc.lexeme);
 				tokens.push_back(acc);
 			}
-			//currentToken = "";
-			//currentState = 0;
 			break;
 		case 3:
 			currentToken += currentChar;
@@ -121,8 +122,6 @@ vector<tokenType> lexer(string words) {
 			acc.lexeme = 1;
 			acc.lexemeName = getLexemeName(acc.lexeme);
 			tokens.push_back(acc);
-			//currentToken = "";
-			//currentState = 0;
 			break;
 		case 5:
 			currentToken += currentChar;
@@ -131,21 +130,18 @@ vector<tokenType> lexer(string words) {
 			currentToken += currentChar;
 			break;
 		case 7:
+			currentToken += currentChar;
 			acc.token = currentToken;
 			acc.lexeme = 2;
 			acc.lexemeName = getLexemeName(acc.lexeme);
 			tokens.push_back(acc);
-			//currentToken = "";
-			//currentState = 0;
 			break;
 		case 8:
 			currentToken += currentChar;
 			acc.token = currentToken;
-			acc.lexeme = 4; //should be 4
+			acc.lexeme = 4; 
 			acc.lexemeName = getLexemeName(acc.lexeme);
 			tokens.push_back(acc);
-			//currentToken = "";
-			//currentState = 0;
 			break;
 		case 9:
 			currentToken += currentChar;
@@ -153,16 +149,12 @@ vector<tokenType> lexer(string words) {
 			acc.lexeme = 3;
 			acc.lexemeName = getLexemeName(acc.lexeme);
 			tokens.push_back(acc);
-			//currentToken = "";
-			//currentState = 0;
 			break;
 		case 10:
 			acc.token = currentToken;
 			acc.lexeme = 5;
 			acc.lexemeName = getLexemeName(acc.lexeme);
 			tokens.push_back(acc);
-			//currentToken = "";
-			//currentState = 0;
 			break;
 		case 11:
 			currentToken += currentChar;
@@ -175,22 +167,18 @@ vector<tokenType> lexer(string words) {
 			acc.lexeme = 6;
 			acc.lexemeName = getLexemeName(acc.lexeme);
 			tokens.push_back(acc);
-			//currentToken = "";
-			//currentState = 0;
 			break;
 		default:
 			break;
 		}
 
+		//will clear token and adds current character to the token if these separators are found
+		//we decided to separate the clearing and adding from the switch statements because it would cause the FSM to confuse certain cases.
 		if ((currentChar == '(' && currentState == 2) || (currentChar == ',' && currentState == 12) || (currentChar == ')' && currentState == 12)) {
 			currentToken = "";
 			currentToken += currentChar;
 		}
 
-		/*if (currentChar = ',' && currentState == 12) {
-			currentToken = "";
-			currentToken += currentChar;
-		}*/
 
 		if ((currentState == 2 && currentToken == "(") || (currentState == 12 && currentToken == ",") || (currentState == 12 && currentToken == ")")) {
 			acc.token = currentToken;
@@ -199,30 +187,16 @@ vector<tokenType> lexer(string words) {
 			tokens.push_back(acc);
 		}
 
-		/*if (currentState == 12 && currentToken == ",") {
-			acc.token = currentToken;
-			acc.lexeme = 3;
-			acc.lexemeName = getLexemeName(acc.lexeme);
-			tokens.push_back(acc);
-		}*/
 
+		//resets the token and state if an end state is found
 		if (currentState == 2 || currentState == 4 || currentState == 7 || currentState == 8 || currentState == 9 || currentState == 10 || currentState == 12) {
 			currentToken = "";
 			currentState = 0;
 		}
-
-	
-		
-		/*if (currentState == 2 || currentState == 4 || currentState == 7 || currentState == 8 || currentState == 9 || currentState == 10 || currentState == 12) {
-			acc.token = currentToken;
-			acc.lexeme = prevState;
-			acc.lexemeName = getLexemeName(acc.lexeme);
-			tokens.push_back(acc);
-		}*/
-
 		
 	}
-	//shortcoming
+	//shortcoming: the program wasn't able to detect a space after "else" so it would break out of the loop.  This if statement
+	//handles that specific problem.
 	if (currentState == 1 && currentToken != "") {
 		acc.token = currentToken;
 		acc.lexeme = 0;
@@ -234,6 +208,7 @@ vector<tokenType> lexer(string words) {
 	return tokens;
 }
 
+//gets the column depending on the current character
 int getFsmCol(char currentChar) {
 
 	if (isalpha(currentChar)) {
@@ -322,6 +297,8 @@ int getFsmCol(char currentChar) {
 
 }
 
+
+//get lexeme name depending on the current state
 string getLexemeName(int lexeme) {
 	switch (lexeme) {
 	case 0:
